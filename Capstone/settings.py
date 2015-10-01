5f4691206ccb9766050ad86710b5b9c3d9ffff31
jesse fitzjarrell <jessecodesportland@gmail.com>
2015-10-01 15:31:53 -0700

    i added comments to header

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -1,3 +1,7 @@
+#Jesse Fitzjarrell 9-30-15
+#Blatent copying of Django Girls blog with some minor modifications. 
+#I did this to learn Django and prove that I can follow a code example
+# I added a jQuery hide show on the comment section and changed the CSS 
 """
 Django settings for Capstone project.
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
+#Jesse Fitzjarrell 9-30-15
+#Blatent copying of Django Girls blog with some minor modifications. 
+#I did this to learn Django and prove that I can follow a code example 
+# I added a jQuery hide show on the comment section and changed the CSS 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Comment
